a79f3863690c2d34a41854673184f95371206748
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-27 15:47:34 +0200

    update

--- main.dart
+++ main.dart
@@ -1,61 +1,34 @@
-main() {
-  for (int i = 0; i < 5; i++) {
-    print('Hello ${i + 1}');
+enum Direction { gauche, droite, devant, derriere }
+
+void main() {
+  var direction = Direction.gauche;
+
+  switch (direction) {
+    case Direction.gauche:
+      print('A gauche !');
+      break;
+    case Direction.droite:
+      print('A droite !');
+      break;
+    case Direction.devant:
+      print('Devant !');
+      break;
+    case Direction.derriere:
+      print('Derriere !');
+      break;
   }
 
-  // List<int> liste = [1, 2, 3];
-  // int result = 0;
+  List<int> values = [1, 2, 4, 5];
+  print(soustraire(15, values));
 
-  // liste.forEach((elem) => result = result + elem);
-  // print(result);
-
-  // List<int> ages = [29, 27, 42];
-  // for (var age in ages) {
-  //   print(age);
-  // }
-
-  // List<int> ages = [29, 27, 42];
-  // // the arguement that forEach expects is
-  // // a *call back*. This function will be called on
-  // // each element
-  // ages.forEach((int age) => print(age));
-
-  List<int> ages = [29, 27, 42];
-
-  // should print 30, 28, 43
-  for (var age in ages) {
-    _addAndPrint(age);
-  }
-
-  // should print 30, 28, 43
-  ages.forEach((int age) => _addAndPrint(age));
-
-  // List<int> liste = [1, 2, 3];
-  // int result = 0;
-
-  // for (var e in liste) {
-  //   result = result + e;
-  // }
-
-  // print(result);
-  List<int> liste = [1, 2, 3];
-
-  while (liste.length != 0) {
-    // Si la liste n'a plus qu'un élément nous stoppons la boucle :
-    if (liste.length == 1) break;
-    print(liste);
-    liste.removeLast();
-  }
-  for (var i = 2; i < 10; i++) {
-    // Nous sautons l'itération si i est pair :
-    if (i % 2 == 0) continue;
-    // Donc seuls les éléments impairs sont ajoutés :
-    liste.add(i);
-  }
-
-  print(liste);
+  List<double> doubleValues = [1.1, 2.231, 3.123, 4.21];
+  print(soustraire(15.0, doubleValues));
 }
 
-void _addAndPrint(int number) {
-  print(1 + number);
+T soustraire<T extends num>(T values, List<T> items) {
+  T x = values;
+  for (var value in items) {
+    x = (x - value) as T;
+  }
+  return x;
 }
